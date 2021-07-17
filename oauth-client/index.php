076f8f0e14242b8e7dd3c5b1d0997878abc3aa46
Boudacher Sylvain <sboudacher@myges.fr>
2021-07-17 20:32:04 +0200

    FB de-comments

--- oauth-client/index.php
+++ oauth-client/index.php
@@ -60,18 +60,18 @@ function handleFbSuccess()
     if ($state !== STATE) {
         throw new RuntimeException("{$state} : invalid state");
     }
-    // https://auth-server/token?grant_type=authorization_code&code=...&client_id=..&client_secret=...
-    // $url = "https://graph.facebook.com/oauth/access_token?grant_type=authorization_code&code={$code}&client_id=" . CLIENT_FBID . "&client_secret=" . CLIENT_FBSECRET."&redirect_uri=https://localhost/fbauth-success";
-    // $result = file_get_contents($url);
-    // $resultDecoded = json_decode($result, true);
-    // ["access_token"=> $token] = $resultDecoded;
-    // $userUrl = "https://graph.facebook.com/me?fields=id,name,email";
-    // $context = stream_context_create([
-    //     'http' => [
-    //         'header' => 'Authorization: Bearer ' . $token
-    //     ]
-    // ]);
-    // echo file_get_contents($userUrl, false, $context);
+    https://auth-server/token?grant_type=authorization_code&code=...&client_id=..&client_secret=...
+    $url = "https://graph.facebook.com/oauth/access_token?grant_type=authorization_code&code={$code}&client_id=" . CLIENT_FBID . "&client_secret=" . CLIENT_FBSECRET."&redirect_uri=https://localhost/fbauth-success";
+    $result = file_get_contents($url);
+    $resultDecoded = json_decode($result, true);
+    ["access_token"=> $token] = $resultDecoded;
+    $userUrl = "https://graph.facebook.com/me?fields=id,name,email";
+    $context = stream_context_create([
+        'http' => [
+            'header' => 'Authorization: Bearer ' . $token
+        ]
+    ]);
+    echo file_get_contents($userUrl, false, $context);
 }
 
 function handleTwitchSuccess() 
